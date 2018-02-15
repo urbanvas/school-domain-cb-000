6442adb9400d917c422ddbd7f42a1446c0e17266
Neil Seejoor <nvseejoor@gmail.com>
2018-02-15 16:21:08 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  attr_accessor :name
+
+  roster = {}
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[name] = grade
+  end
+
+end
